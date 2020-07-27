0bc19b1b006a3df52c8d9f5053a99c4230e53066
sathya <karpagasathya@gmail.com>
2020-07-27 01:09:15 -0500

    html routes defined

--- server.js
+++ server.js
@@ -6,9 +6,12 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-let routes = require("./routes/workout_routes.js");
+require("./routes/html-routes")(app);
+require("./routes/api-routes")(app);
 
-app.use(routes);
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+});
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
